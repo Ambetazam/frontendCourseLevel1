688dea2cbd59c3d8dbf70e9fc68741652f60ad7e
ambet <ambetazam@gmail.com>
2026-09-11 13:32:02 -0400

    Update class rooms session exercices for javascript

---
+++ bornAge.js
@@ -0,0 +1,7 @@
+let name = prompt("Ingrese su nombre:");
+let yearBorn = parseInt(prompt("Ingrese su anio de nacimiento:"));
+let actualYears = year - yearBorn
+
+const year = parseInt(2026);
+
+console.log(`Bienvenido ${name} actualmente tienes ${actualYears}`);
